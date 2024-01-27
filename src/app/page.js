5053bce153bb839d02f6a10851dc2b4b17f482de
jmmdev <tales.fever@hotmail.com>
2024-01-27 21:36:33 +0100

    Core values correctly displayed

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,88 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import {IoSunnyOutline, IoCloudyOutline, IoRainyOutline, IoThunderstormOutline, IoSnowOutline, IoReorderThreeOutline, IoWaterOutline, IoArrowForwardOutline, } from 'react-icons/io5';
 
 export default function Home() {
+  const key = "220bf77b081743862a50f764cf8773c8"
+  const [text, setText] = useState('')
+  const [city, setCity] = useState(null)
+
+  const GetWeatherIcon = () => {
+    switch(city.weather[0].main) {
+      case('Clear'):
+        return <IoSunnyOutline />
+        break;
+      case('Clouds'):
+        return <IoCloudyOutline />
+        break;
+      case('Drizzle'):
+      case('Rain'):
+        return <IoRainyOutline />
+        break;
+      case('Thunderstorm'):
+        return <IoThunderstormOutline />
+        break;
+      case('Snow'):
+        return <IoSnowOutline />
+        break;
+      default:
+        return <IoReorderThreeOutline />
+    }
+  }
+
+  const getCityData = () => {
+    if (text.length > 0){  
+      fetch("https://api.openweathermap.org/data/2.5/weather?q="+ text + "&units=metric&appid=" + key).then(
+        response => {
+          response.json().then(data => {
+            setCity(data)
+          })
+        }
+      )
+    }
+  }
+
+  const GetCityInfo = () => {
+    if (city)
+      return (
+        <div className={styles['city-data']}>
+          <div className={styles['left-block']}>
+            <div className={styles.temp}>
+              <GetWeatherIcon />
+              <p>{`${Math.round(city.main.temp)}°`}</p>
+            </div>
+            <p className={styles.description}>{formatDescription(city.weather[0].description)}</p>
+          </div>
+          <div className={styles['right-block']}>
+            <p>{`Min/Max: ${Math.round(city.main.temp_min)}°/${Math.round(city.main.temp_max)}°`}</p>
+            <div>
+              <IoWaterOutline />
+              <p>{`Humidity: ${Math.round(city.main.humidity)}%`}</p>
+            </div>
+            <div>
+              <p>{`Wind: ${Math.round(city.wind.speed)} Km/h`}</p>
+              <div style={{transform: `rotate(${city.wind.deg}deg)`}}>
+                <IoArrowForwardOutline />
+              </div>
+            </div>
+          </div>
+        </div>
+        )
+    return null 
+  }
+
+  const formatDescription = text => {
+    const firstChar = text.charAt(0).toUpperCase()
+    return firstChar + text.slice(1)
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <input value={text} onChange={e => setText(e.target.value)} type="text"/>
+      <GetCityInfo />
+      <button onClick={() => getCityData(text)}>DALE</button>
     </main>
   );
 }
